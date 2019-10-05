912eca839a2885725a603f9f52ef25794614e130
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-05 17:13:15 +0530

    Working with Touchable Components

--- rm-complete-guide/App.js
+++ rm-complete-guide/App.js
@@ -7,16 +7,22 @@ export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
   const addGoalHandler = (goalTitle) => {
-    setCourseGoals((currentGoals) => [...currentGoals, { key: Math.random().toString(), value: goalTitle }]);
+    setCourseGoals((currentGoals) => [...currentGoals, { id: Math.random().toString(), value: goalTitle }]);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
   };
 
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
-        keyExtractor={(item) => item.key}
+        keyExtractor={(item) => item.id}
         data={courseGoals}
-        renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+        renderItem={(itemData) => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
       />
     </View>
   );
--- rm-complete-guide/components/GoalItem.js
+++ rm-complete-guide/components/GoalItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const GoalItem = ({title}) => {
+const GoalItem = ({ id, title, onDelete }) => {
   return (
-    <View style={styles.listItems}>
-      <Text>{title}</Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.8} onPress={onDelete.bind(this, id)}>
+      <View style={styles.listItems}>
+        <Text>{title}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-    listItems: { padding: 10, marginVertical: 10, backgroundColor: '#ccc', borderColor: '#000', borderWidth: 1 },
-})
+  listItems: { padding: 10, marginVertical: 10, backgroundColor: '#ccc', borderColor: '#000', borderWidth: 1 },
+});
 
 export default GoalItem;
