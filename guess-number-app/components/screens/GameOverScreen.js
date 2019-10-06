087de5e8345dedf0ac516bab2b8d248447403345
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-06 21:54:50 +0530

    Adding images and Text component

--- guess-number-app/components/screens/GameOverScreen.js
+++ guess-number-app/components/screens/GameOverScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Button, Image, Text } from 'react-native';
+import BodyText from '../BodyText';
+import TitleText from '../TitleText';
+import Colors from '../../constants/colors';
 
 const GameOverScreen = ({ guessRounds, userNumber, onRestart }) => {
   return (
     <View style={styles.screen}>
-      <Text>Game is Over!</Text>
-      <Text>Number of Rounds: {guessRounds}</Text>
-      <Text>Number was: {userNumber}</Text>
+      <TitleText>Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image source={require('../../assets/success.png')} style={styles.image} resizeMode='cover' />
+      </View>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed <Text style={styles.highlight}>{guessRounds}</Text> rounds to guess the number{' '}
+          <Text style={styles.highlight}>{userNumber}</Text>.
+        </BodyText>
+      </View>
       <Button title='NEW GAME' onPress={onRestart} />
     </View>
   );
@@ -17,6 +27,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: '#000',
+    overflow: 'hidden',
+    marginVertical: 30,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginVertical: 15,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold',
   },
 });
 
