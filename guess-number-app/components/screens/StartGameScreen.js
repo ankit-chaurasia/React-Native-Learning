07d6b608fdf2b595a3b1a9259abb5c52d58e0238
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-06 16:13:50 +0530

    reset and confirm

--- guess-number-app/components/screens/StartGameScreen.js
+++ guess-number-app/components/screens/StartGameScreen.js
@@ -6,9 +6,28 @@ import Input from '../Input';
 
 const StartGameScreen = () => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState('');
+
   const numberInputHandler = (inputText) => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''));
   };
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (chosenNumber == NaN || chosenNumber <= 0 || chosenNumber > 99) {
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(parseInt(enteredValue));
+    setEnteredValue('');
+  };
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.screen}>
@@ -27,13 +46,14 @@ const StartGameScreen = () => {
           />
           <View style={styles.buttonContainer}>
             <View style={styles.button}>
-              <Button title='Reset' onPress={() => {}} color={Colors.accent} />
+              <Button title='Reset' onPress={resetInputHandler} color={Colors.accent} />
             </View>
             <View style={styles.button}>
-              <Button title='Confirm' onPress={() => {}} color={Colors.primary} />
+              <Button title='Confirm' onPress={confirmInputHandler} color={Colors.primary} />
             </View>
           </View>
         </Card>
+        {confirmed && <Text style={styles.title}>Chosen Number is: {selectedNumber}</Text>}
       </View>
     </TouchableWithoutFeedback>
   );
