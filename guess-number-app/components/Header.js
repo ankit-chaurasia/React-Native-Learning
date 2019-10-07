409a18e0fb6e5392ec63c76d83da19cfdaffeb71
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-07 18:18:00 +0530

    Working with Platform.select() and Platform checks

--- guess-number-app/components/Header.js
+++ guess-number-app/components/Header.js
@@ -5,22 +5,27 @@ import TitleText from './TitleText';
 
 const Header = ({ title }) => {
   return (
-    <View style={styles.header}>
+    <View style={{ ...styles.headerBase, ...Platform.select({ ios: styles.headerIOS, android: styles.headerAndroid }) }}>
       <TitleText style={styles.headerTitle}>{title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+  },
+  headerIOS: {
+    backgroundColor: '#fff',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
   },
   headerTitle: {
     color: Platform.OS === 'ios' ? Colors.primary : '#fff',
