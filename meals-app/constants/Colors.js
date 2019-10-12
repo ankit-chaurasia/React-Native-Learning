1415d7f4d7281da61ecafbe66684145038089d54
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-13 00:01:01 +0530

    Configuring the Header with Navigation Options

---
+++ meals-app/constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+  primaryColor: '#4a148c',
+  accentColot: '#ff6f00',
+};
--- meals-app/screens/CategoriesScreen.js
+++ meals-app/screens/CategoriesScreen.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Platform } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
-
-const renderGridItem = (itemData) => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() =>
+          props.navigation.navigate({
+            routeName: 'CategoryMeals',
+          })
+        }
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return <FlatList keyExtractor={(item) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />;
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+  },
+  headerTintColor: Platform.OS === 'android' ? '#fff' : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
