95c0b630c38830ba48f7bdb661f289221cd1f3aa
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-13 23:26:37 +0530

    Setting Icons and Configuring Tabs

--- meals-app/constants/Colors.js
+++ meals-app/constants/Colors.js
@@ -1,4 +1,4 @@
 export default {
   primaryColor: '#4a148c',
-  accentColot: '#ff6f00',
+  accentColor: '#ff6f00',
 };
--- meals-app/navigation/MealsNavigator.js
+++ meals-app/navigation/MealsNavigator.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
@@ -32,9 +34,31 @@ const MealsNavigator = createStackNavigator(
   },
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator,
-  Favourites: FavouritesScreen,
-});
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />;
+        },
+      },
+    },
+    Favourites: {
+      screen: FavouritesScreen,
+      navigationOptions: {
+        tabBarLabel: 'Favourites!',
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
+        },
+      }
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  },
+);
 
 export default createAppContainer(MealsFavTabNavigator);
