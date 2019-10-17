fbb1951a2337244bd834bbe1b5e492bb989d00ca
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-17 22:41:53 +0530

    Adding the MealDetail Screen Content

--- meals-app/screens/MealDetailScreen.js
+++ meals-app/screens/MealDetailScreen.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Button, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { MEALS } from '../data/dummy-data';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import DefaultText from '../components/DefaultText';
+
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
-  const { title } = MEALS.find((meal) => meal.id === mealId);
+  const { duration, complexity, affordibility, imageUrl, ingredients, steps } = MEALS.find((meal) => meal.id === mealId);
   return (
-    <View style={styles.screen}>
-      <Text>{title}</Text>
-      <Text>MealDetailScreen</Text>
-      <Button title='Go Back to Categories' onPress={() => props.navigation.popToTop()} />
-    </View>
+    <ScrollView>
+      <Image source={{ uri: imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{duration}m</DefaultText>
+        <DefaultText>{complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{affordibility.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {ingredients.map((ingredient) => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
@@ -36,10 +56,26 @@ MealDetailScreen.navigationOptions = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
