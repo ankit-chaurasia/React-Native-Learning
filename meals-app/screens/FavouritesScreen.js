117ea2a926175a151294e0b2af9ea04418262880
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2019-10-19 17:13:25 +0530

    Rendering a Fallback Text

--- meals-app/screens/FavouritesScreen.js
+++ meals-app/screens/FavouritesScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { Item, HeaderButtons } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 import MealList from '../components/MealList';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import DefaultText from '../components/DefaultText';
 
 const FavouritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favouriteMeals);
-
-  return <MealList listData={favMeals} navigation={props.navigation} />;
+  if (favMeals && favMeals.length) {
+    return <MealList listData={favMeals} navigation={props.navigation} />;
+  } else {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favourite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 };
 
 FavouritesScreen.navigationOptions = ({ navigation }) => {
@@ -20,5 +29,13 @@ FavouritesScreen.navigationOptions = ({ navigation }) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default FavouritesScreen;
